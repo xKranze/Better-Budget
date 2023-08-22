c0c86404042e02cd912e0e4dad0fb34c6f9ce770
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-08-21 21:00:23 -0700

    income schema done

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/programming-workouts');
-        console.log('Successfully connected to DB');
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('Successfully connected to DB')
     } catch (error) {
         console.log('Error connecting to DB');
     }
