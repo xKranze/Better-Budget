d81da6da367e6c03ed9860741218af407db14b69
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-08-22 19:56:17 -0700

    added validation for income

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,12 +1,12 @@
-const IncomeSchema= require("../models/incomeModel")
+const IncomeSchema = require("../models/incomeModel")
 
 
 exports.addIncome = async (req, res) => {
     // console.log(req.body);
     //Destructuring the .body data by doing the following
-    const {title, amount, category, description, date}  = req.body
+    const { title, amount, category, description, date } = req.body
 
-    //create an instance of data above into incomeSchema from models
+    //create an instance of data above into incomeSchema from models.  use the schema to create instances of income records.
     const income = IncomeSchema({
         title,
         amount,
@@ -14,5 +14,21 @@ exports.addIncome = async (req, res) => {
         description,
         date
     })
-    console.log(income)
+    // console.log(income)
+
+    try {
+        //validations
+        if (!title || !category || !description || !date) {
+            return res.status(400).json({ message: 'All fields need too be filled and are required!' })
+        }
+        //if amount is less than or equal to 0 or not a number.
+        if (amount <= 0 || !amount === 'number') {
+            return res.status(400).json({ message: 'The amount must be a positive number!' })
+        }
+        //save income instance to DB
+        await income.save()
+        res.status(200).json({message: 'Income added!$$$'})
+    } catch (error) {
+
+    }
 }
