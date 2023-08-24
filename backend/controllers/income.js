07559462c9d9a8d246159217ca66f21156d9d837
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-08-23 20:13:28 -0700

    Added CRUD for income

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,6 +1,6 @@
 const IncomeSchema = require("../models/incomeModel")
 
-
+//ADD income
 exports.addIncome = async (req, res) => {
     // console.log(req.body);
     //Destructuring the .body data by doing the following
@@ -27,8 +27,33 @@ exports.addIncome = async (req, res) => {
         }
         //save income instance to DB
         await income.save()
-        res.status(200).json({message: 'Income added!$$$'})
+        res.status(200).json({ message: 'Income added!$$$' })
     } catch (error) {
-        res.status(500).json({message: 'Invalid Data!!'})
+        res.status(500).json({ message: 'ERROR Invalid Data!' })
     }
+    console.log(income)
+}
+
+//GET Income
+exports.getIncomes = async (req, res) =>{
+    try {
+        //Fetch all records from the IncomeSchema collection and sorts them based on the createdAt field in descending order,  This will sort last created item and sort to the top of list, by default last created item will be at bottom of list. Meaning that the newest records will appear at the beginning of the incomes array.
+        const incomes = await IncomeSchema.find().sort({createdAt: -1})
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({message: 'ERROR Invalid Data!'})
+    }
+}
+
+
+//DELETE Income by id
+exports.deleteIncomes = async (req, res) =>{
+   const {id} = req.params;
+   IncomeSchema.findByIdAndDelete(id)
+   .then((income) =>{
+    res.status(200).json({message: 'Income successfully deleted!'})
+   })
+   .catch((err) =>{
+    res.status(500).json({message: 'ERROR Invalid Data!'})
+   }) 
 }
