187e579fea23b0254f7d4c537a5feff62bcac118
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-08-31 20:56:10 -0700

    added expense schema and route

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -1,20 +1,20 @@
-const IncomeSchema = require("../models/incomeModel")
+const ExpenseSchema = require("../models/ExpenseModel")
 
 //ADD income
-exports.addIncome = async (req, res) => {
+exports.addExpense = async (req, res) => {
     // console.log(req.body);
     //Destructuring the .body data by doing the following
     const { title, amount, category, description, date } = req.body
 
-    //create an instance of data above into incomeSchema from models.  use the schema to create instances of income records.
-    const income = IncomeSchema({
+    //create an instance of data above into expenseSchema from models.  use the schema to create instances of expense records.
+    const expense = ExpenseSchema({
         title,
         amount,
         category,
         description,
         date
     })
-    // console.log(income)
+    // console.log(expense)
 
     try {
         //validations
@@ -25,21 +25,21 @@ exports.addIncome = async (req, res) => {
         if (amount <= 0 || !amount === 'number') {
             return res.status(400).json({ message: 'The amount must be a positive number!' })
         }
-        //save income instance to DB
-        await income.save()
-        res.status(200).json({ message: 'Income added!$$$' })
+        //save expense instance to DB
+        await expense.save()
+        res.status(200).json({ message: 'Expense added!$$$' })
     } catch (error) {
         res.status(500).json({ message: 'ERROR Invalid Data!' })
     }
-    console.log(income)
+    console.log(expense)
 }
 
 //GET Income
-exports.getIncomes = async (req, res) =>{
+exports.getExpense = async (req, res) =>{
     try {
-        //Fetch all records from the IncomeSchema collection and sorts them based on the createdAt field in descending order,  This will sort last created item and sort to the top of list, by default last created item will be at bottom of list. Meaning that the newest records will appear at the beginning of the incomes array.
-        const incomes = await IncomeSchema.find().sort({createdAt: -1})
-        res.status(200).json(incomes)
+        //Fetch all records from the ExpenseSchema collection and sorts them based on the createdAt field in descending order,  This will sort last created item and sort to the top of list, by default last created item will be at bottom of list. Meaning that the newest records will appear at the beginning of the incomes array.
+        const incomes = await ExpenseSchema.find().sort({createdAt: -1})
+        res.status(200).json(expense)
     } catch (error) {
         res.status(500).json({message: 'ERROR Invalid Data!'})
     }
@@ -47,12 +47,12 @@ exports.getIncomes = async (req, res) =>{
 
 
 //DELETE Income by id
-exports.deleteIncomes = async (req, res) =>{
+exports.deleteIExpense = async (req, res) =>{
    const {id} = req.params;
 //    console.log(req.params);
-   IncomeSchema.findByIdAndDelete(id)
+ExpenseSchema.findByIdAndDelete(id)
    .then((income) =>{
-    res.status(200).json({message: 'Income successfully deleted!'})
+    res.status(200).json({message: 'Expense successfully deleted!'})
    })
    .catch((err) =>{
     res.status(500).json({message: 'ERROR Invalid Data!'})
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,3 +1,4 @@
+const { addExpense, getExpense, deleteIExpense } = require('../controllers/expense');
 const { addIncome, getIncomes, deleteIncomes } = require('../controllers/income');
 
 const router = require('express').Router();
@@ -8,4 +9,11 @@ router.post('/add-income', addIncome)
     .get('/get-incomes', getIncomes)
     //delete request using get id for params
     .delete('/delete-income/:id', deleteIncomes)
+
+    //get the addExpense function
+    .post('/add-expense', addExpense)
+    .get('/get-expense', getExpense)
+    //delete request using get id for params
+    .delete('/delete-expense/:id', deleteIExpense)
+
 module.exports = router
