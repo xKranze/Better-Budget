e3238f3775b8163c0d145c313612d2273ee1cc00
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-09-02 10:14:04 -0700

    added layouts

--- budget-frontend/src/App.js
+++ budget-frontend/src/App.js
@@ -1,9 +1,35 @@
+import { styled } from "styled-components";
+import bg from './img/bg.png'
+import {MainLayout} from './styles/Layouts'
+
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <AppStyled bg={bg} className="App">
+   <MainLayout>
+    <h1>
+     
+      Tetsing
+    </h1>
+   </MainLayout>
+    </AppStyled>
   );
 }
+//access background image as a prop
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
 
 export default App;
